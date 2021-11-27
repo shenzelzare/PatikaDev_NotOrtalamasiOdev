69f6f5c92576f11336912ad13cb9ce9b58f3a4cd
shenzelzare <abc@example.com>
2021-11-27 13:36:23 +0300

    init commit

---
+++ src/NotOrtalamasi.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int matematik, fizik, kimya, turkce, tarih, muzik, toplam ;
+        double ortalama;
+        System.out.print("matematik notunuz :");
+        matematik = scanner.nextInt();
+        System.out.print("fizik notunuz : ");
+        fizik = scanner.nextInt();
+        System.out.print("kimya notunuz :");
+        kimya = scanner.nextInt();
+        System.out.print("turkce notunuz : ");
+        turkce = scanner.nextInt();
+        System.out.print("tarih notunuz :");
+        tarih = scanner.nextInt();
+        System.out.print("muzik notunuz : ");
+        muzik = scanner.nextInt();
+         toplam = matematik+fizik+kimya+turkce+tarih+muzik;
+         ortalama = (double) (toplam/6.0);
+        System.out.println("not ortalamasi : "+ ortalama);
+        if (ortalama>60){
+            System.out.println("Sinifi gecti");
+        }
+        else
+        {
+            System.out.println("Sinifta kaldi");
+        }
+
+    }
+}
